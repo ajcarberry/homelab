becdd463138a65cb5bc76333e4c8897894bd8ae9
Alex Carberry <alex.carberry@RDU-AL-16321.carberries.net>
2018-11-18 01:47:13 -0500

    fix list syntax error on ec2 module output

--- terraform/modules/common/aws/ec2/debian/output.tf
+++ terraform/modules/common/aws/ec2/debian/output.tf
@@ -3,9 +3,9 @@
 # =================================
 
 output "public_ip" {
-  value = "${aws_instance.debian_ec2.public_ip}"
+  value = "${aws_instance.debian_ec2.*.public_ip}"
 }
 
 output "private_ip" {
-  value = "${aws_instance.debian_ec2.private_ip}"
+  value = "${aws_instance.debian_ec2.*.private_ip}"
 }
--- terraform/modules/common/aws/ec2/ubuntu/output.tf
+++ terraform/modules/common/aws/ec2/ubuntu/output.tf
@@ -3,9 +3,9 @@
 # =================================
 
 output "public_ip" {
-  value = "${aws_instance.ubuntu_ec2.public_ip}"
+  value = "${aws_instance.ubuntu_ec2.*.public_ip}"
 }
 
 output "private_ip" {
-  value = "${aws_instance.ubuntu_ec2.private_ip}"
+  value = "${aws_instance.ubuntu_ec2.*.private_ip}"
 }
